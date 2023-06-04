713c69a5add935cbc5c81c80d4ad2341b94592c4
Pascal Reeb <pascal@reeb.io>
2023-06-04 11:26:05 +0200

    feat(py): init structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # aionuki
+
+A Python asyncio library for interacting with the [Nuki bridge](https://nuki.io/bridge/) local HTTP-Api.
+
+Original written for [Home Assistant](https://home-assistant.io).
---
+++ setup.py
@@ -0,0 +1,31 @@
+"""Setup for aionuki."""
+from setuptools import find_packages, setup
+
+LONG_DESC = open("README.md").read()
+PACKAGES = find_packages(exclude=["tests", "tests.*"])
+REQUIREMENTS = list(val.strip() for val in open("requirements.txt"))
+MIN_PY_VERSION = "3.8"
+
+setup(
+    name="aionuki",
+    version="0.0.1",
+    license="Apache License 2.0",
+    url="https://github.com/home-assistant-libs/aionuki",
+    author="Pascal Reeb",
+    author_email="pascal@reeb.io",
+    description="Python module to talk to Nuki bridges.",
+    long_description=LONG_DESC,
+    long_description_content_type="text/markdown",
+    packages=PACKAGES,
+    zip_safe=True,
+    platforms="any",
+    install_requires=REQUIREMENTS,
+    python_requires=f">={MIN_PY_VERSION}",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
